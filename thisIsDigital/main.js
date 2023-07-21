b851cf99e42b902392614250064bcf1db2344ee3
mateen27 <khanmateen2713@gmail.com>
2023-07-21 12:07:36 +0530

    thisisDigital Landing Page

---
+++ thisIsDigital/main.js
@@ -0,0 +1,30 @@
+// targetting image 1
+gsap.from('#img1' , {
+    delay : 0.4 , 
+    duration  : 1 , 
+    y : 60 , 
+    opacity : 0
+});
+
+// targetting image 2
+gsap.from('#img2' , {
+    delay : 0.4 , 
+    duration  : 1 , 
+    x : 60 , 
+    opacity : 0
+})
+
+// targetting image 3
+gsap.from('#img3' , {
+    delay : 0.4 , 
+    duration  : 1 , 
+    y : -60 , 
+    opacity : 0
+})
+
+// targetting main h1
+gsap.from('#main h1' , {
+    delay : 0.4 , 
+    duration  : 1 ,
+    opacity : 0 , 
+})
